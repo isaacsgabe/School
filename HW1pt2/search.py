a0ad70ca78dc3f77e1aaf87bbfc2baf2e2caed7d
Gabe Isaacs <gabeisaacs99@gmail.com>
2022-09-13 16:41:00 -0400

    HW1

---
+++ HW1pt2/search.py
@@ -0,0 +1,93 @@
+#search
+
+import state
+import frontier
+total = 0
+
+push = 0
+pop = 0
+depth = 0
+
+def search(n):
+    global push
+    global pop
+    global depth
+    s=state.create(n)
+    f=frontier.create(s)
+    while not frontier.is_empty(f):
+        s=frontier.remove(f)
+        f[3] = f[3]+1
+        if state.is_target(s):
+            push += f[2]
+            pop += f[3]
+            depth += len(s[1])
+            return [s, f[1]]
+        ns=state.get_next(s)
+        for i in ns:
+            frontier.insert(f,i)
+            f[2] = f[2]+1
+    return 0
+
+# for i in range(10000):
+#     x = search(3)
+#     total += x[1]
+# total/=10000
+# print(total)
+
+for x in range(100):
+    print(x)
+    search(4)
+push /= 100
+pop/=100
+depth/=100
+print("that amount pushed on the frontier is " + str(push))
+print("that amount popped off the frontier is " + str(pop))
+print("the total depth is: " + str(depth))
+
+
+"""
+#######################################
+part 2
+#######################################
+a) with hdistance1 unweighted
+
+4x4 with hdistance1 regular
+    that amount pushed on the frontier is 62169.06
+    that amount popped off the frontier is 29742.61
+    the total depth is: 16.27
+
+
+############################################
+a) with hdistance2 unweighted
+
+
+4x4 with hdistance2 regular
+    that amount pushed on the frontier is 4113.56
+    that amount popped off the frontier is 2048.22
+    the total depth is: 10.1
+
+####################
+2*hdistance1.... weighted
+
+
+4x4
+    that amount pushed on the frontier is 28048.16
+    that amount popped off the frontier is 13319.44
+    the total depth is: 10.7
+
+####################
+2*hdistance2.... weighted
+
+4x4
+    that amount pushed on the frontier is 3315.18
+    that amount popped off the frontier is 1650.37
+    the total depth is: 11.38
+
+
+
+
+"""
+
+
+
+
